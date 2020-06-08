2bf182ae05887e4b3006972eddbc2796bef03fcf
majun <1747236434@qq.com>
2020-06-08 20:22:15 +0800

    初始化系统

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get '/signup', to: 'users#new'
+
+  root "static_pages#home"
+end
